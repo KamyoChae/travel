e66f1fe5be09efece65f394eedacc8229d3141e8
kamyochae <1465678596@qq.com>
2018-10-28 13:44:16 +0800

    菜单小图标

---
+++ src/pages/components/Icon.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="icon-swiper">
+
+    <swiper :options="swiperOption" >
+        <swiper-slide  v-for="(page, index) of pages" :key="index">
+            <img class="swiper-img" :src="item.imgUrl" alt="">
+        </swiper-slide>
+
+        <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+</div>
+</template>
+
+<script>
+export default {
+    name:"Icon",
+    data(){
+        return {
+            pages:[],
+            iconList:[{iconUrl}], // axios传过来的icon菜单
+        }
+    },
+    methods:{
+
+    }
+
+}
+</script>
+
+<style lang='stylus' scoped>
+
+
+</style>
