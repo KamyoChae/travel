2a82e74bb214f66db64636a33fdd1a60ee71b104
kamyochae <1465678596@qq.com>
2018-10-30 21:32:01 +0800

    优化ajax

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -9,53 +9,57 @@
 </template>
 
 <script>
-
-import HomeHeader from './components/Header'
-import HomeSwiper from './components/Swiper'
-import HomeIcon from './components/Icon'
-import HomePopular from './components/Popular'
-import HomeWeekend from './components/Weekend'
-import axios from "axios"
+import HomeHeader from "./components/Header";
+import HomeSwiper from "./components/Swiper";
+import HomeIcon from "./components/Icon";
+import HomePopular from "./components/Popular";
+import HomeWeekend from "./components/Weekend";
+import axios from "axios";
 export default {
   name: "Home",
-  components:{
+  components: {
+    HomeHeader,
+    HomeSwiper,
+    HomeIcon,
+    HomePopular,
+    HomeWeekend
+  },
 
-      HomeHeader,
-      HomeSwiper,
-      HomeIcon,
-      HomePopular,
-      HomeWeekend,
+  data() {
+    return {
+      city: "",
+      swiperList: [],
+      iconList: [],
+      weekendList: [],
+      popuList: [],
+      lastCity:""
+    };
+  },
+  methods: {
+    getHomInfo() {
+      axios.get("/api/index.json?city=" + this.$store.city).then(this.infoSucc);
+    },
+    infoSucc(res) {
+      if (res.data) {
+        let myRes = res.data.data;
+        this.city = myRes.city;
+        this.swiperList = myRes.swiperList;
+        this.iconList = myRes.iconList;
+        this.weekendList = myRes.weekendList;
+        this.popuList = myRes.recommendList;
+      }
+    }
   },
   mounted() {
-      this.getHomInfo()
+    this.lastCity = this.$store.city
+    this.getHomInfo();
   },
-  data(){
-      return {
-          city:"",
-          swiperList:[],
-          iconList:[],
-          weekendList:[],
-          popuList:[]
+  activated() {
+      if(this.lastCity !== this.$store.city ){
+          this.lastCity = this.$store.city
+          this.getHomInfo();
       }
   },
-  methods:{
-      getHomInfo () {
-          axios.get("/api/index.json")
-                .then(this.infoSucc)
-      },
-      infoSucc(res){
-          if(res.data){
-              let myRes = res.data.data
-              this.city = myRes.city
-              this.swiperList = myRes.swiperList
-              this.iconList = myRes.iconList
-              this.weekendList = myRes.weekendList
-              this.popuList = myRes.recommendList
-          }
-          console.log(res.data.data)
-      }
-  }
-
 };
 </script>
 
